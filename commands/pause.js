6bb8813f21afd265056fd198133ee2623e9e6bdd
Nguyen Duy Bach <59746573+ndbiaw@users.noreply.github.com>
2021-01-10 01:28:10 +0700

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -2,10 +2,10 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
   name: "pause",
-  description: "Pause the currently playing music",
+  description: "Tạm dừng bản nhạc hiện tại.",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("Không có bản nhạc nào đang phát.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     if (queue.playing) {
