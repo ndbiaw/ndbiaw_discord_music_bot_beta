fa0bc626bbc32f07a6a1729c0bae492427a1a9a0
Nguyen Duy Bach <59746573+ndbiaw@users.noreply.github.com>
2021-01-10 01:49:38 +0700

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -3,15 +3,15 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 module.exports = {
   name: "skip",
   aliases: ["s"],
-  description: "Skip the currently playing song",
+  description: "Bỏ qua bài nhạc đang phát",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue)
-      return message.reply("There is nothing playing that I could skip for you.").catch(console.error);
+      return message.reply("Không có bài nhạc nào đang được phát.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     queue.playing = true;
     queue.connection.dispatcher.end();
-    queue.textChannel.send(`${message.author} ⏭ skipped the song`).catch(console.error);
+    queue.textChannel.send(`${message.author} ⏭ đã bỏ qua bài nhạc.`).catch(console.error);
   }
 };
