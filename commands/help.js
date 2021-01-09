fb3867859cf2f5f7ba6fc449de928ccb53b7dcc0
Nguyen Duy Bach <59746573+ndbiaw@users.noreply.github.com>
2021-01-10 01:15:38 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,13 +3,13 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "help",
   aliases: ["h"],
-  description: "Display all commands and descriptions",
+  description: "Hiển thị tất cả mô tả của lệnh.",
   execute(message) {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
       .setTitle(`${message.client.user.username} Help`)
-      .setDescription("List of all commands")
+      .setDescription("Danh sách tất cả lệnh:")
       .setColor("#F8AA2A");
 
     commands.forEach((cmd) => {
