290f64d2778231e4a1e8e9cdf620da7f37182ee0
Nguyen Duy Bach <59746573+ndbiaw@users.noreply.github.com>
2021-01-10 01:44:43 +0700

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -3,18 +3,18 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 module.exports = {
   name: "resume",
   aliases: ["r"],
-  description: "Resume currently playing music",
+  description: "Phát tiếp bài nhạc.",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("Không có bài nhạc nào đang phát.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     if (!queue.playing) {
       queue.playing = true;
       queue.connection.dispatcher.resume();
-      return queue.textChannel.send(`${message.author} ▶ resumed the music!`).catch(console.error);
+      return queue.textChannel.send(`${message.author} ▶ tiếp tục phát nhạc!`).catch(console.error);
     }
 
-    return message.reply("The queue is not paused.").catch(console.error);
+    return message.reply("Hàng đợi không bị tạm dừng.").catch(console.error);
   }
 };
