fd3282c7fd1a7b4670f5eba267a670b26a12e081
Nguyen Duy Bach <59746573+ndbiaw@users.noreply.github.com>
2021-01-10 01:39:19 +0700

    Update pruning.js

--- commands/pruning.js
+++ commands/pruning.js
@@ -9,7 +9,7 @@ try {
 
 module.exports = {
   name: "pruning",
-  description: "Toggle pruning of bot messages",
+  description: "Điều chỉnh tin nhắn bot.",
   execute(message) {
     if (!config) return;
     config.PRUNING = !config.PRUNING;
@@ -17,11 +17,11 @@ module.exports = {
     fs.writeFile("./config.json", JSON.stringify(config, null, 2), (err) => {
       if (err) {
         console.log(err);
-        return message.channel.send("There was an error writing to the file.").catch(console.error);
+        return message.channel.send("Lỗi bất định.").catch(console.error);
       }
 
       return message.channel
-        .send(`Message pruning is ${config.PRUNING ? "**enabled**" : "**disabled**"}`)
+        .send(`Trạng thái: ${config.PRUNING ? "**Bật**" : "**Tắt**"}`)
         .catch(console.error);
     });
   }
