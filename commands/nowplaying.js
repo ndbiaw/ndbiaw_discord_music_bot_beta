0e8ea23713801b1c1ca9c2f068e0100d5b0bb280
Nguyen Duy Bach <59746573+ndbiaw@users.noreply.github.com>
2021-01-10 01:27:23 +0700

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -3,17 +3,17 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = {
   name: "np",
-  description: "Show now playing song",
+  description: "Hiển thị bài hát đang phát.",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("Không có bài hát nào đang phát.").catch(console.error);
 
     const song = queue.songs[0];
     const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
     const left = song.duration - seek;
 
     let nowPlaying = new MessageEmbed()
-      .setTitle("Now playing")
+      .setTitle("Đang phát:")
       .setDescription(`${song.title}\n${song.url}`)
       .setColor("#F8AA2A")
       .setAuthor(message.client.user.username);
