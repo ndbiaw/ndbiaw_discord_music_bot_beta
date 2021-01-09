f7cf1a58b238c28cbcc1ff060dc66b13d556be78
Nguyen Duy Bach <59746573+ndbiaw@users.noreply.github.com>
2021-01-10 01:24:17 +0700

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -4,18 +4,18 @@ const lyricsFinder = require("lyrics-finder");
 module.exports = {
   name: "lyrics",
   aliases: ["ly"],
-  description: "Get lyrics for the currently playing song",
+  description: "Lấy lời bài hát đang phát.",
   async execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is nothing playing.").catch(console.error);
+    if (!queue) return message.channel.send("Không có bài hát nào đang phát.").catch(console.error);
 
     let lyrics = null;
 
     try {
       lyrics = await lyricsFinder(queue.songs[0].title, "");
-      if (!lyrics) lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+      if (!lyrics) lyrics = `Không tìm thấy lời bài hát cho ${queue.songs[0].title}.`;
     } catch (error) {
-      lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+      lyrics = `Không tìm thất lời bài hát cho ${queue.songs[0].title}.`;
     }
 
     let lyricsEmbed = new MessageEmbed()
