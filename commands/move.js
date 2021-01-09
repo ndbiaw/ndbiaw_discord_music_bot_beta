ad0d2bed4f32bdabc104fbe8973fcf8d4ccc5c2d
Nguyen Duy Bach <59746573+ndbiaw@users.noreply.github.com>
2021-01-10 01:26:12 +0700

    Update move.js

--- commands/move.js
+++ commands/move.js
@@ -4,20 +4,20 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 module.exports = {
   name: "move",
   aliases: ["mv"],
-  description: "Move songs around in the queue",
+  description: "Di chuyển bài nhạc trong hàng đợi.",
   execute(message, args) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is no queue.").catch(console.error);
+    if (!queue) return message.channel.send("Không có hàng đợi nào.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     if (!args.length) return message.reply(`Usage: ${message.client.prefix}move <Queue Number>`);
-    if (isNaN(args[0]) || args[0] <= 1) return message.reply(`Usage: ${message.client.prefix}move <Queue Number>`);
+    if (isNaN(args[0]) || args[0] <= 1) return message.reply(`Sử dụng: ${message.client.prefix}move <Queue Number>`);
 
     let song = queue.songs[args[0] - 1];
 
     queue.songs = move(queue.songs, args[0] - 1, args[1] == 1 ? 1 : args[1] - 1);
     queue.textChannel.send(
-      `${message.author} 🚚 moved **${song.title}** to ${args[1] == 1 ? 1 : args[1] - 1} in the queue.`
+      `${message.author} 🚚 di chuyển **${song.title}** đến ${args[1] == 1 ? 1 : args[1] - 1} trong hàng đợi.`
     );
   }
 };
