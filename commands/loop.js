85630c2f4b02bd54e16013f94d6d2254a8f68653
Nguyen Duy Bach <59746573+ndbiaw@users.noreply.github.com>
2021-01-10 01:20:34 +0700

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -3,14 +3,14 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 module.exports = {
   name: "loop",
   aliases: ["l"],
-  description: "Toggle music loop",
+  description: "Điều chỉnh lặp lại.",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("Không có bài nhạc đang phát nào.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     // toggle from false to true and reverse
     queue.loop = !queue.loop;
-    return queue.textChannel.send(`Loop is now ${queue.loop ? "**on**" : "**off**"}`).catch(console.error);
+    return queue.textChannel.send(`Lặp lại đang ${queue.loop ? "**Bật**" : "**Tắt**"}`).catch(console.error);
   }
 };
